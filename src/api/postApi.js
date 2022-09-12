f5d0861cb3c0ee835cf685cce1f5ba4cf03d2f60
박인석 <inseok@isp.local>
2022-09-13 00:17:09 +0900

    fix: Add missing brackets At postApi.js

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -2,22 +2,26 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const postApi = createApi({
   reducerPath: 'postApi',
+  tagTypes: ['Posts'],
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.REACT_APP_API_URL,
   }),
   endpoints: (builder) => ({
     getPosts: builder.query({
-      query: () => 'posts'
+      query: () => 'posts',
+      providesTags: [{ type: 'Posts', id: 'LIST'}]
     }),
     getPostById: builder.query({
-      query: (postId) => `posts/${postId}`
+      query: (postId) => `posts/${postId}`,
+      providesTags: (result, error, postId) => [{type: 'Posts', id: postId}]
     }),
     createPost: builder.mutation({
       query: ({data}) => ({
         url: 'posts',
         method: 'POST',
         body: data,
-      })
+      }),
+      invalidatesTags: (result) => result ? [{type: 'Posts', id: 'LIST'}] : []
     })
   })
 })
